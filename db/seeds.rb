274beca1d7c6d5f669ccb641329993b23a4c45c5
djskim27 <djs.kim27@gmail.com>
2017-11-15 15:17:53 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Title.create({Emp_No: 10001, Job_Title:"Padawon"})
+Title.create({Emp_No: 10002, Job_Title:"Business-Savvy Dude"})
+Title.create({Emp_No: 10003, Job_Title:"Genius Dude"})
